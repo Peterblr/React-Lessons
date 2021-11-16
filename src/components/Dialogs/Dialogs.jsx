c242f46005851a022a5718ca894d5c2f32fb2cc9
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-16 16:07:58 +0300

    add style messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import s from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Name1
+                </div>
+                <div className={s.dialog}>
+                    Name2
+                </div>
+                <div className={s.dialog}>
+                    Name3
+                </div>
+                <div className={s.dialog}>
+                    Name4
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Hi!</div>
+            </div>
         </div>
-    );
+    )
 };
 
 export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
-import s from "./Profile.module.css";
 
 function Profile() {
   return (
     <div >
       <div>
-        <img
-          src={
-            ""
-            // "https://thelibertarianrepublic.com/wp-content/uploads/2017/04/WALL-E-1.jpg"
-          }
-          alt="img"
-          width="900px"
-        />
+        {/*<img*/}
+        {/*  src={*/}
+        {/*    ""*/}
+        {/*    // "https://thelibertarianrepublic.com/wp-content/uploads/2017/04/WALL-E-1.jpg"*/}
+        {/*  }*/}
+        {/*  alt="img"*/}
+        {/*  // width="900px"*/}
+        {/*/>*/}
       </div>
       <div>ava + description</div>
       <MyPosts />
