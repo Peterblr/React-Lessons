4878741a7172a708b80dbf36728873948b355205
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-16 11:45:13 +0300

    add router-dom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+
+import {BrowserRouter, Route} from "react-router-dom";
+
+
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path="/Profile" component={Profile}/>
+                    <Route path="/Dialogs" component={Dialogs}/>
+                    <Route path="/Music" component={Music}/>
+                    <Route path="/News" component={News}/>
+                    <Route path="/Settings" component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from "./Profile.module.css";
 
 function Profile() {
   return (
-    <div className={s.content}>
+    <div >
       <div>
         <img
           src={
